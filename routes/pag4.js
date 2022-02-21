5c088422454470d8011a414759110ecdc6a2c269
HenricoGV06 <100131560+HenricoGV06@users.noreply.github.com>
2022-02-21 10:07:22 -0300

    Add files via upload

---
+++ routes/pag4.js
@@ -0,0 +1,11 @@
+module.exports = (app)=>{
+    //rendeizar a view pagina4.ejs
+    app.get('/localizacao',(req,res)=>{
+        res.render('pagina4.ejs')
+    })
+
+    //abrir o Google Maps
+    app.get('/mapa', (req,res)=>{
+        res.redirect("http://maps.google.com")
+    } )
+}
